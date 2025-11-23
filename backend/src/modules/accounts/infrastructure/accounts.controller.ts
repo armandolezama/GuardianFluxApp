3534318433a0160de04c3f7e14843ad6f554157b
Armando Lezama <armando.lezama.psic@gmail.com>
2025-11-23 03:25:13 -0600

    fix(ts): completed get account

--- backend/src/modules/accounts/infrastructure/accounts.controller.ts
+++ backend/src/modules/accounts/infrastructure/accounts.controller.ts
@@ -1,10 +1,10 @@
 import { Controller, Get, UseGuards, Req } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Roles } from '../../auth/infrastructure/roles.decorator';
+import { RolesGuard } from '../../auth/infrastructure/roles.guard';
+import { Role } from '../../users/domain/role.enum';
 import { GetAccountsForUserUseCase } from '../application/get-accounts-for-user.usecase';
 import { Request } from 'express';
-import { RolesGuard } from '../../auth/infrastructure/roles.guard';
-import { Roles } from '../../auth/infrastructure/roles.decorator';
-import { Role } from '../../users/domain/role.enum';
 
 @UseGuards(AuthGuard('jwt'), RolesGuard)
 @Roles(Role.CUSTOMER, Role.CUSTOMER_DEMO)
@@ -16,7 +16,7 @@ export class AccountsController {
 
   @Get('me')
   async me(@Req() req: Request) {
-    const user = req.user as any; // luego lo haces Passport typed
+    const user = req.user as any;
     const result = await this.getAccountsForUser.execute({
       userId: user.userId,
     });
