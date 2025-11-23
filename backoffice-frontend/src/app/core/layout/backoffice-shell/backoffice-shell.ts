cbfebe132c1985662eb32f5a478d59800d82ecc5
Armando Lezama <armando.lezama.psic@gmail.com>
2025-11-23 14:40:24 -0600

    fix(ts): removed hard href

--- backoffice-frontend/src/app/core/layout/backoffice-shell/backoffice-shell.ts
+++ backoffice-frontend/src/app/core/layout/backoffice-shell/backoffice-shell.ts
@@ -1,15 +1,13 @@
 import { Component, inject, Input, OnChanges } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { AuthService } from '../../auth/services/auth.service';
-
+import { AuthService } from '../.././auth/services/auth.service';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { ThemeService } from '../../theme/theme.service';
-
 
 export type BackofficeRole = 'ADMIN' | 'MONITOR';
 
@@ -25,13 +23,15 @@ export type BackofficeRole = 'ADMIN' | 'MONITOR';
   templateUrl: './backoffice-shell.html',
   styleUrl: './backoffice-shell.scss',
 })
-export class BackofficeShell implements OnChanges{
+export class BackofficeShell implements OnChanges {
   private auth = inject(AuthService);
   private themeSvc = inject(ThemeService);
+  private router = inject(Router);
 
   @Input({ required: true }) role!: BackofficeRole;
 
-  menu: Array<{label: string; path: string}> = [];
+  // ✅ propiedad estable
+  menu: Array<{ label: string; path: string }> = [];
 
   ngOnChanges() {
     this.menu = this.role === 'ADMIN'
@@ -39,11 +39,11 @@ export class BackofficeShell implements OnChanges{
       : [{ label: 'Movements', path: '/monitor/movements' }];
   }
 
-  trackByPath = (_: number, item: {path: string}) => item.path;
+  trackByPath = (_: number, item: { path: string }) => item.path;
 
   logout() {
     this.auth.logout();
-    location.href = '/login';
+    this.router.navigateByUrl('/login'); // ✅ sin reload duro
   }
 
   toggleTheme() {
