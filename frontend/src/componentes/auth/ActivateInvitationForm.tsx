52e6d460599b5cd1da1ac59fb78f46c2dacbf33e
Mons1214 <mons.gorsor@gmail.com>
2025-11-24 23:31:06 -0600

    States were added to the ActivateInvitationForm file

--- frontend/src/componentes/auth/ActivateInvitationForm.tsx
+++ frontend/src/componentes/auth/ActivateInvitationForm.tsx
@@ -1,13 +1,22 @@
 import { useState } from 'react';
-import { Box, Button, TextField, Typography  } from '@mui/material';
+import { Box, Button, TextField, Typography } from '@mui/material';
 
 
 export function ActivateInvitationForm() {
   const [code, setCode] = useState('');
-  const[error, setError] = useState('');
+  const [error, setError] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState(''); 
 
   const handleSubmit = (e: React.FormEvent) => {
+    console.log(
+{
+code, email, password, name
+}
+)
     e.preventDefault();
+
     if (!code.trim()) {
       setError("El código es obligatorio");
       return;
@@ -15,29 +24,61 @@ export function ActivateInvitationForm() {
     setError('');
   };
 
+
+
   return (
     <Box component="form" onSubmit={handleSubmit}>
-        <TextField 
-          label="Código de invitación"
-          value={code}
-          onChange={(e) => setCode(e.target.value)}
-            fullWidth
-            margin="normal"
-        />
+      <TextField
+        label="Código de invitación"
+        value={code}
+        onChange={(e) => setCode(e.target.value)}
+        fullWidth
+        margin="normal"
+      />
 
-        {error && (
-          <Typography variant="caption" color="error">
-            {error}
-          </Typography>
-        )}  
+      {error && (
+        <Typography variant="caption" color="error">
+          {error}
+        </Typography>
+      )}
 
-        <Button
-          variant="contained"
-          type='submit'
-          >
-            Validar Invitación
-        </Button>
+      <TextField
+        label="Nombre"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        fullWidth
+        margin="normal">
+        Name
+      </TextField>
+
+
+      <TextField
+        label="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        fullWidth
+        margin="normal">
+        Email
+      </TextField>
+
+
+      <TextField
+        label="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        fullWidth
+        margin="normal">
+        Password
+      </TextField>
+
+
+      <Button
+        variant="contained"
+        type='submit'
+      >
+        Validar Invitación
+      </Button>
     </Box>
-    );
+  );
 }
 
