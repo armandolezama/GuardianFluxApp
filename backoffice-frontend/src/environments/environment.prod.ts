119b8080b689d0d12ba40388a8e75a80b5c47974
armandolezama <armando.lezama.psic@gmail.com>
2025-11-26 00:47:26 -0600

    fix(ts): added env var for production

--- backoffice-frontend/src/environments/environment.prod.ts
+++ backoffice-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // Si quieres forzar que en prod use el mismo origen:
-  apiBaseUrl: '', // dejamos que lo resuelva el helper
+  apiBaseUrl: '__API_BASE_URL__',
 };
